74deb58cb17458e7ef2d5758d5d10371752dc962
Jack <34525842+chillandgrill@users.noreply.github.com>
2018-01-17 20:27:13 +0000

    Update Windows.py

--- Encrypter/Windows.py
+++ Encrypter/Windows.py
@@ -2,17 +2,18 @@ import os
 from Encrypter import Decrypt
 from Encrypter import Encrypt
 
+# Get a list of files in the current working directory (This is the part that limits this script to windows, as Unix OSes handle applications differently)
 file_list = os.listdir(os.getcwd())
 
 for file in file_list:
     if file.endswith(".txt"):
-        working = open(file, "r")
+        working = open(file, "r") # Open the file in read mode, this is the part of code that limits the program to ANSI only.
         raw_file_text = working.read()
         working.close()
 
-        working = open(file, "w")
+        working = open(file, "w") # Open the file in write mode. This completely wipes the file, and this causes data loss in the case of a bad read.
 
-        if raw_file_text.startswith("#encrypted"):
+        if raw_file_text.startswith("#encrypted"): # Check to see if the current file is encrypted or not
             Decrypt.init_string(raw_file_text)
             working.write(Decrypt.construct_file())
 
